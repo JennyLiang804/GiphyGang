16b49a2442abe88e2006b6ebe69477ee2d035dfb
Jenny Liang <jennylove804@gmail.com>
2018-09-13 12:38:54 -0400

    footer

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # GiphyGang
-# GifTastic
 
 ### Overview
 
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var topics = "";
+var results = "";
 
 $("button").on("click", function(){
 
@@ -22,7 +22,7 @@ $.ajax({
             var gifImage = $("<img>");
 
             var gifImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
-            gifImage.addClass("card-img-top gif");
+            gifImage.addClass("gif");
             gifImage.attr("data-still", results[i].images.fixed_height_still.url);
             gifImage.attr("data-animate", results[i].images.fixed_height.url);
             gifImage.attr("data-state", "still")
@@ -50,3 +50,41 @@ $(document).on( "click", ".gif", function () {
 
     });
 
+    function renderButtons() {
+
+        // Deletes the Topics prior to adding new Topic
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+        // Loops through the array of movies
+        for (var i = 0; i < results.length; i++) {
+
+          // Then dynamicaly generates buttons for each topic in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adds a class of topic to our button
+          a.addClass("topic btn btn-outline-success");
+          // Added a data-attribute
+          a.attr("data-name", results[i]);
+          // Provided the initial button text
+          a.text(results[i]);
+          // Added the button to the buttons-view div
+          $("#buttons-view").append(a);
+        }
+      }
+
+      // This function handles events where the add topic button is clicked
+      $("#add-topic").on("click", function(event) {
+          console.log("add topic button clicked");
+        event.preventDefault();
+        // This line of code will grab the input from the textbox
+        var topic = $("#topic-input").val().trim();
+
+        // The topic from the textbox is then added to our array
+        $(results).push(topic);
+
+        // Calling renderButtons which handles the processing of our topic array
+        renderButtons();
+      });
+
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
